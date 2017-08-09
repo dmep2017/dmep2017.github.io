86be1fa41a4a49c7b9e72222fcf4d6f7b1b259ec
HU Pili <hpl1989@gmail.com>
2017-08-09 18:39:14 +0800

    Add slides meta data

---
+++ slides.js
@@ -0,0 +1,25 @@
+slides = [{
+    name: 'Day 1 - Frontend & HTML',
+    id: '1BE959TyjkuupOdOximVH26BGBUvZqKDWpJFd5xCbwI0',
+}, {
+    name: 'Day 2 - HTML/CSS',
+    id: '1rb75kJWpJzIcLBXZeMIKZWJWSfEpYXiXnRk_kxaQnYo'
+}, {
+    name: 'Day 3 - Responsive Web Design',
+    id: '1_QdnTujbuJQiJN0Guva3UkWoy29dUCAFEUvecK432nk'
+}, {
+    name: 'Day 4 - Javascript & eCharts',
+    id: '1DSpfrc-Y5HRZQBlfVjNUxs2n7N2LRTe8MiNKOP_SvXU'
+}, {
+    name: 'Day 5 - Javascript & jQuery',
+    id: '1YgQn8c_MOUjzdHLW2kgCXHFtlVg9BBJjAiidpTe_lH4'
+}, {
+    name: 'Day 6 - JSON & API',
+    id: '1xRLRTnyg5Ovt_uuUSZPKwodZbfLA7ntFJd0Ff5tzXhM'
+}, {
+    name: 'Day 7 - D3',
+    id: '1MRU7pJr1x_zCwTAgLmPxVzJQw_As-nP0CVzk6XJRHk8'
+}, {
+    name: 'Day 8 - Mapping',
+    id: '17Xt7QPz5QK_fMwM7QtztpK6Nj_-7_ufJ9qkSc407GCE'
+}]
