efcc626913da2116b20af4263493a9c94d0e2929
HU Pili <hpl1989@gmail.com>
2017-08-18 22:32:41 +0800

    Add images

--- slides.js
+++ slides.js
@@ -1,31 +1,41 @@
 slides = [{
     name: 'Day 1 - Frontend & HTML',
     id: '1BE959TyjkuupOdOximVH26BGBUvZqKDWpJFd5xCbwI0',
+    image: 'day1-overall.png',
 }, {
     name: 'Day 2 - HTML/CSS',
-    id: '1rb75kJWpJzIcLBXZeMIKZWJWSfEpYXiXnRk_kxaQnYo'
+    id: '1rb75kJWpJzIcLBXZeMIKZWJWSfEpYXiXnRk_kxaQnYo',
+    image: 'day2-css.png',
 }, {
     name: 'Day 3 - Responsive Web Design',
-    id: '1_QdnTujbuJQiJN0Guva3UkWoy29dUCAFEUvecK432nk'
+    id: '1_QdnTujbuJQiJN0Guva3UkWoy29dUCAFEUvecK432nk',
+    image: 'day3-rwd.png',
 }, {
     name: 'Day 4 - Javascript & eCharts',
-    id: '1DSpfrc-Y5HRZQBlfVjNUxs2n7N2LRTe8MiNKOP_SvXU'
+    id: '1DSpfrc-Y5HRZQBlfVjNUxs2n7N2LRTe8MiNKOP_SvXU',
+    image: 'day4-echarts.png',
 }, {
     name: 'Day 5 - Javascript & jQuery',
-    id: '1YgQn8c_MOUjzdHLW2kgCXHFtlVg9BBJjAiidpTe_lH4'
+    id: '1YgQn8c_MOUjzdHLW2kgCXHFtlVg9BBJjAiidpTe_lH4',
+    image: 'day5-jquery.png',
 }, {
     name: 'Day 6 - JSON & API',
-    id: '1xRLRTnyg5Ovt_uuUSZPKwodZbfLA7ntFJd0Ff5tzXhM'
+    id: '1xRLRTnyg5Ovt_uuUSZPKwodZbfLA7ntFJd0Ff5tzXhM',
+    image: 'day6-json.png',
 }, {
     name: 'Day 7 - D3',
-    id: '1MRU7pJr1x_zCwTAgLmPxVzJQw_As-nP0CVzk6XJRHk8'
+    id: '1MRU7pJr1x_zCwTAgLmPxVzJQw_As-nP0CVzk6XJRHk8',
+    image: 'day7-D3.png',
 }, {
     name: 'Day 8 - Mapping',
-    id: '17Xt7QPz5QK_fMwM7QtztpK6Nj_-7_ufJ9qkSc407GCE'
+    id: '17Xt7QPz5QK_fMwM7QtztpK6Nj_-7_ufJ9qkSc407GCE',
+    image: 'day8-mapping.png',
 }, {
     name: 'Day 9 - Graph',
-    id: '1ezsqMgqAxA1g6pZd4qqasBLP2vwDPswGeSLns6V3YQc'
+    id: '1ezsqMgqAxA1g6pZd4qqasBLP2vwDPswGeSLns6V3YQc',
+    image: 'day9-graph.png',
 }, {
     name: 'Day 10 - Miscellaneous',
-    id: '1oPGj9rbcYZEUEGFjorll8iA-tTUbUqJMwgJ6bBw86E8'
+    id: '1oPGj9rbcYZEUEGFjorll8iA-tTUbUqJMwgJ6bBw86E8',
+    image: 'day10-misc.png',
 }]
