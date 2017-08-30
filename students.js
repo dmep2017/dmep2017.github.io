0f34191c9606ffe7ce844a21eb7371fc366a2eb7
HU Pili <hpl1989@gmail.com>
2017-08-30 15:57:18 +0800

    Add student final project links

--- students.js
+++ students.js
@@ -8,126 +8,140 @@ students = [{
         "name": "陈明",
         "url": "https://chenming117.github.io",
         "githubHandle": "chenming117",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/13947539?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/13947539?v=4",
+        "final": "https://chenming117.github.io/xi/"
     },
     {
         "name": "贾雨朦",
         "url": "https://jiayumengshirley.github.io",
         "githubHandle": "jiayumengshirley",
-        "avatarUrl": "https://avatars0.githubusercontent.com/u/30227817?v=4"
+        "avatarUrl": "https://avatars0.githubusercontent.com/u/30227817?v=4",
+        "final": "https://jiayumengshirley.github.io/html/terrorismattack.html/finalproject/"
     },
     {
         "name": "蓝星宇",
         "url": "https://olivialan.github.io",
         "githubHandle": "olivialan",
-        "avatarUrl": "https://avatars2.githubusercontent.com/u/11660552?v=4"
+        "avatarUrl": "https://avatars2.githubusercontent.com/u/11660552?v=4",
+        "final": "https://olivialan.github.io/lyrics/index.html"
     },
     {
         "name": "蒋津津",
         "url": "https://sherryjiangjinjin.github.io",
         "githubHandle": "sherryjiangjinjin",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/30222976?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/30222976?v=4",
+        "final": "https://sherryjiangjinjin.github.io/"
     },
     {
         "name": "曾奕珊",
         "url": "https://zengyishan.github.io",
         "githubHandle": "zengyishan",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/16832621?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/16832621?v=4",
+        "final": "https://zengyishan.github.io/"
     },
     {
         "name": "吴扬",
         "url": "https://young331.github.io",
         "githubHandle": "young331",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/30365539?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/30365539?v=4",
+        "final": "https://young331.github.io/finalproject/",
     },
     {
         "name": "张凌云",
         "url": "https://lyrtclouds.github.io",
         "githubHandle": "lyrtclouds",
-        "avatarUrl": "https://avatars2.githubusercontent.com/u/30581176?v=4"
+        "avatarUrl": "https://avatars2.githubusercontent.com/u/30581176?v=4",
+        "final": "https://lyrtclouds.github.io/"
     },
     {
         "name": "苏洁",
         "url": "https://susieca.github.io",
         "githubHandle": "susieca",
-        "avatarUrl": "https://avatars2.githubusercontent.com/u/30209658?v=4"
+        "avatarUrl": "https://avatars2.githubusercontent.com/u/30209658?v=4",
+        "final": "https://susieca.github.io/Maxim/index.html"
     },
     {
         "name": "梁轶琳",
         "url": "https://elaineliang.github.io",
         "githubHandle": "elaineliang",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/30134849?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/30134849?v=4",
+        "final": "ElaineLiang.github.io/mywork/index.html"
     },
     {
         "name": "王文浩",
         "url": "https://winhows5.github.io",
         "githubHandle": "winhows5",
-        "avatarUrl": "https://avatars0.githubusercontent.com/u/20163093?v=4"
+        "avatarUrl": "https://avatars0.githubusercontent.com/u/20163093?v=4",
+        "final": "https://winhows5.github.io/filmsInTenYears/"
     },
     {
         "name": "于文澜",
         "url": "https://Wenlan5.github.io",
         "githubHandle": "Wenlan5",
-        "avatarUrl": "https://avatars0.githubusercontent.com/u/30205739?v=4"
+        "avatarUrl": "https://avatars0.githubusercontent.com/u/30205739?v=4",
+        "final": "https://wenlan5.github.io/BAT_Recruitment/"
     },
     {
         "name": "王敏",
         "url": "https://wangmin1221.github.io",
         "githubHandle": "wangmin1221",
-        "avatarUrl": "https://avatars2.githubusercontent.com/u/24621759?v=4"
+        "avatarUrl": "https://avatars2.githubusercontent.com/u/24621759?v=4",
+        "final": "https://wangmin1221.github.io/",
     },
     {
         "name": "叶霄麒",
         "url": "https://siukay.github.io",
         "githubHandle": "siukay",
-        "avatarUrl": "https://avatars3.githubusercontent.com/u/30222947?v=4"
+        "avatarUrl": "https://avatars3.githubusercontent.com/u/30222947?v=4",
+        "final": "siukay.github.io/final/"
     },
     {
         "name": "陈雪",
         "url": "https://snowflakes176x.github.io",
         "githubHandle": "snowflakes176x",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/30209711?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/30209711?v=4",
+        "final": "https://414027639.wixsite.com/linda"
     },
     {
         "name": "张晓雨",
         "url": "https://mystica1994.github.io",
         "githubHandle": "mystica1994",
-        "avatarUrl": "https://avatars3.githubusercontent.com/u/15998677?v=4"
+        "avatarUrl": "https://avatars3.githubusercontent.com/u/15998677?v=4",
+        "final": "http://suo.im/Si4Uj"
     },
     {
         "name": "李嘉欣",
         "url": "https://jessielee1230.github.io",
         "githubHandle": "jessielee1230",
-        "avatarUrl": "https://avatars2.githubusercontent.com/u/30223032?v=4"
+        "avatarUrl": "https://avatars2.githubusercontent.com/u/30223032?v=4",
+        "final": "https://jessielee1230.github.io/HPV-Vaccine/",
     },
     {
         "name": "JuliannaWu",
         "url": "https://juliannawqy.github.io",
         "githubHandle": "juliannawqy",
-        "avatarUrl": "https://avatars2.githubusercontent.com/u/25113115?v=4"
-    },
-    {
-        "name": "梁轶琳",
-        "url": "https://elaineliang.github.io",
-        "githubHandle": "elaineliang",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/30134849?v=4"
+        "avatarUrl": "https://avatars2.githubusercontent.com/u/25113115?v=4",
+        "final": "https://juliannawqy.github.io/filmscript/"
     },
     {
         "name": "赵欣",
         "url": "https://cicindyzhao.github.io",
         "githubHandle": "cicindyzhao",
-        "avatarUrl": "https://avatars3.githubusercontent.com/u/30162987?v=4"
+        "avatarUrl": "https://avatars3.githubusercontent.com/u/30162987?v=4",
+        "final": "https://cicindyzhao.github.io/finalproject/index.html"
     },
     {
         "name": "邵海涵",
         "url": "https://brandworld.github.io",
         "githubHandle": "brandworld",
-        "avatarUrl": "https://avatars1.githubusercontent.com/u/24471015?v=4"
+        "avatarUrl": "https://avatars1.githubusercontent.com/u/24471015?v=4",
+        "final": "brandworld.github.io"
     },
     {
         "name": "陈嘉慧",
         "url": "https://trishsky.github.io/",
         "githubHandle": "trishsky",
-        "avatarUrl": "https://avatars0.githubusercontent.com/u/12014359?v=4"
+        "avatarUrl": "https://avatars0.githubusercontent.com/u/12014359?v=4",
+        "final": "https://trishsky.github.io/project/index.html"
     }
 ]
